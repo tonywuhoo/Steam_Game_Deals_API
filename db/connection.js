f170ccc0c15cca29d874871d896870f996461190
tonywuhoo <76459334+tonywuhoo@users.noreply.github.com>
2022-10-20 15:09:54 -0400

    create a connection.js

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+import mongoose, { mongo } from "mongoose";
+import chalk from "chalk";
+//A tool to help view server data
+
+mongoose.set("returnOriginal", false)
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/NBAAPI")
+  .catch((error) => {
+    console.error("There's an error ", error)
+  })
+
+mongoose.connection.on("disconnected", () => {
+  console.log(chalk.bold("Disconnected from MongoDB"))
+})
+
+mongoose.connection.on("error", (error) => {
+  console.log(chalk.red("Connection error", error))
+})
+
+export default mongoose.connection
